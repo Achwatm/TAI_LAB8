80ea157f3808d617e094d5c0c07932cd406b5608
kocurass@gmail.com <k.kapalka@hotmail.com>
2019-04-26 16:42:38 +0200

    first commit

---
+++ src/main/java/com/tailab8/tailab8/Tailab8Application.java
@@ -0,0 +1,13 @@
+package com.tailab8.tailab8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tailab8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tailab8Application.class, args);
+	}
+
+}
